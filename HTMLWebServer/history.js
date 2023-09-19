cd443e0c76bee4a84b31813f8b9569873cae5c17
opedromartins <pmartins939@gmail.com>
2023-09-19 12:41:07 -0300

    Added history page

---
+++ HTMLWebServer/history.js
@@ -0,0 +1,92 @@
+// Initialize Chart.js variables
+const ctxCO2 = document.getElementById("co2Chart").getContext("2d");
+const ctxAltitude = document.getElementById("altitudeChart").getContext("2d");
+const ctxTemperature = document.getElementById("temperatureChart").getContext("2d");
+const ctxHumidity = document.getElementById("humidityChart").getContext("2d");
+
+const dataTemplate = {
+    labels: [],
+    datasets: [{
+        label: "",
+        data: [],
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+        fill: false,
+    }]
+};
+
+const co2Data = JSON.parse(JSON.stringify(dataTemplate));
+co2Data.datasets[0].pointRadius = 1;
+co2Data.datasets[0].pointHitRadius = 0;
+
+const altitudeData = JSON.parse(JSON.stringify(dataTemplate));
+altitudeData.datasets[0].pointRadius = 1;
+altitudeData.datasets[0].pointHitRadius = 0;
+
+const temperatureData = JSON.parse(JSON.stringify(dataTemplate));
+temperatureData.datasets[0].pointRadius = 1;
+temperatureData.datasets[0].pointHitRadius = 0;
+
+const humidityData = JSON.parse(JSON.stringify(dataTemplate));
+humidityData.datasets[0].pointRadius = 1;
+humidityData.datasets[0].pointHitRadius = 0;
+
+co2Data.datasets[0].label = "CO2";
+altitudeData.datasets[0].label = "Altitude";
+temperatureData.datasets[0].label = "Temperature";
+humidityData.datasets[0].label = "Humidity";
+
+const co2Chart = new Chart(ctxCO2, { options: { scales: { y: { min: 0, max: 100 } } },
+    type: "line",
+    data: co2Data,
+});
+
+const altitudeChart = new Chart(ctxAltitude, { options: { scales: { y: { min: 0, max: 1000 } } },
+    type: "line",
+    data: altitudeData,
+});
+
+const temperatureChart = new Chart(ctxTemperature, { options: { scales: { y: { min: 0, max: 100 } } },
+    type: "line",
+    data: temperatureData,
+});
+
+const humidityChart = new Chart(ctxHumidity, { options: { scales: { y: { min: 0, max: 100 } } },
+    type: "line",
+    data: humidityData,
+});
+
+// Function to fetch sensor data and update the charts
+const fetchDataAndUpdateCharts = async () => {
+    try {
+        const response = await fetch("http://192.168.137.54/");
+        const data = await response.json();
+
+        const now = new Date().toLocaleTimeString();
+
+        // Update CO2 Chart
+        co2Data.labels.push(now);
+        co2Data.datasets[0].data.push(data.mq135);
+        co2Chart.update();
+
+        // Update Altitude Chart
+        altitudeData.labels.push(now);
+        altitudeData.datasets[0].data.push(data.altitude);
+        altitudeChart.update();
+
+        // Update Temperature Chart
+        temperatureData.labels.push(now);
+        temperatureData.datasets[0].data.push(data.temperature);
+        temperatureChart.update();
+
+        // Update Humidity Chart
+        humidityData.labels.push(now);
+        humidityData.datasets[0].data.push(data.humidity);
+        humidityChart.update();
+
+    } catch (error) {
+        console.error("Failed to fetch sensor data:", error);
+    }
+};
+
+setInterval(fetchDataAndUpdateCharts, 3000);
